e50f8f05f6c0035195d645984da724fd6f98c913
Ashish Jain <jain@Ashishs-MacBook-Air.local>
2025-09-30 12:35:37 +0530

    Commit all the changes

--- frontend/src/Components/AboutMV.jsx
+++ frontend/src/Components/AboutMV.jsx
@@ -15,10 +15,10 @@ const AboutUs = () => {
                 <div className="md:w-2/2">
                     <h2 className="text-3xl md:text-4xl font-bold text-primary-green text-center mb-6">About Mind Vriksha</h2>
                     <p className="text-gray-700 text-lg mb-4">
-                        Mind Vriksha, was founded in 2012 by Dr. Rohit Garg, MD (Psychiatry), and Senior Resident at AIIMS Delhi, with a simple yet powerful vision: to make mental health care accessible, compassionate, and stigma-free.
+                       Mind Vriksha is a chain of mental health clinics. It was founded in 2012 by Dr. Rohit Garg with a simple yet powerful idea: to make quality and compassionate mental health care accessible and stigma-free.
                     </p>
                     <p className="text-gray-700 text-lg mb-4">
-                        We are a growing chain of mental health clinics. Our first and flagship center in GK2 has earned a reputation as one of Delhi NCR’s most trusted destinations for mental health and de-addiction care. After years of learning, refining our protocols, and strengthening our team, we expanded in 2025 with three new clinics across Delhi-NCR - CR Park, Noida, and Faridabad.
+                        Our first and flagship center in GK2 has earned a reputation as one of Delhi NCR’s most trusted destinations for mental health and de-addiction care. After years of learning, refining our protocols, and strengthening our team, we expanded in 2025 with three new clinics across Delhi-NCR - Chittaranjan Park, Noida, and Faridabad.
                     </p>
                     <p className="text-gray-700 text-lg mb-4">
                         At Mind Vriksha, we believe that every mind is like a tree — beautiful, complex, and capable of growth when nurtured with the right care. We have supported over two lakh lives facing mental health and addiction challenges, more than half through charitable initiatives. We combine evidence-based practices with deep empathy to ensure that every person feels safe, understood, and respected.
